43d99402ff4bd777e4eb885bde789b2805fa528a
Markson <88060437+Markson-bit@users.noreply.github.com>
2022-11-08 22:08:20 +0100

    Most of features added: Saving to file, reading from file, adding tasks. Deleting soon

--- TaskManagerForms/Form1.Designer.cs
+++ TaskManagerForms/Form1.Designer.cs
@@ -28,25 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.drawtask = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.addtask = new System.Windows.Forms.Button();
             this.deltask = new System.Windows.Forms.Button();
-            this.savenexit = new System.Windows.Forms.Button();
+            this.tasksbox = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // drawtask
-            // 
-            this.drawtask.Location = new System.Drawing.Point(308, 35);
-            this.drawtask.Name = "drawtask";
-            this.drawtask.Size = new System.Drawing.Size(147, 69);
-            this.drawtask.TabIndex = 0;
-            this.drawtask.Text = "Show Tasks";
-            this.drawtask.UseVisualStyleBackColor = true;
-            this.drawtask.Click += new System.EventHandler(this.drawtask_Click);
             // 
             // addtask
             // 
-            this.addtask.Location = new System.Drawing.Point(308, 110);
+            this.addtask.Location = new System.Drawing.Point(403, 271);
             this.addtask.Name = "addtask";
             this.addtask.Size = new System.Drawing.Size(147, 69);
             this.addtask.TabIndex = 1;
@@ -56,7 +48,7 @@
             // 
             // deltask
             // 
-            this.deltask.Location = new System.Drawing.Point(308, 185);
+            this.deltask.Location = new System.Drawing.Point(625, 271);
             this.deltask.Name = "deltask";
             this.deltask.Size = new System.Drawing.Size(147, 69);
             this.deltask.TabIndex = 2;
@@ -64,37 +56,60 @@
             this.deltask.UseVisualStyleBackColor = true;
             this.deltask.Click += new System.EventHandler(this.deltask_Click);
             // 
-            // savenexit
+            // tasksbox
             // 
-            this.savenexit.Location = new System.Drawing.Point(308, 260);
-            this.savenexit.Name = "savenexit";
-            this.savenexit.Size = new System.Drawing.Size(147, 69);
-            this.savenexit.TabIndex = 3;
-            this.savenexit.Text = "Save\'n Exit";
-            this.savenexit.UseVisualStyleBackColor = true;
-            this.savenexit.Click += new System.EventHandler(this.savenexit_Click);
+            this.tasksbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tasksbox.FormattingEnabled = true;
+            this.tasksbox.ItemHeight = 26;
+            this.tasksbox.Location = new System.Drawing.Point(12, 12);
+            this.tasksbox.Name = "tasksbox";
+            this.tasksbox.Size = new System.Drawing.Size(365, 420);
+            this.tasksbox.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(403, 399);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(369, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(547, 383);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Input task here:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.savenexit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tasksbox);
             this.Controls.Add(this.deltask);
             this.Controls.Add(this.addtask);
-            this.Controls.Add(this.drawtask);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "TaskManager";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button drawtask;
         private System.Windows.Forms.Button addtask;
         private System.Windows.Forms.Button deltask;
-        private System.Windows.Forms.Button savenexit;
+        private System.Windows.Forms.ListBox tasksbox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- TaskManagerForms/Form1.cs
+++ TaskManagerForms/Form1.cs
@@ -7,34 +7,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace TaskManagerForms
 {
+
     public partial class Form1 : Form
     {
+        string filePath = Application.StartupPath + @"\tasks.txt";
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void drawtask_Click(object sender, EventArgs e)
-        {
-            
+            tasksbox.Items.Clear();
+            string[] content = File.ReadAllLines(filePath);
+            foreach (string inside in content)
+            {
+                tasksbox.Items.Add(inside);
+            }
         }
 
         private void addtask_Click(object sender, EventArgs e)
         {
-
+            if (textBox1.Text.Length>0)
+            {
+                if (tasksbox.Items.Contains(textBox1.Text))
+                {
+                    DialogResult result = MessageBox.Show("Exactly same tasks already exist. Are you sure you want to add it again?", "Action needed.", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                    if (result == DialogResult.Yes)
+                    {
+                        tasksbox.Items.Add(textBox1.Text);
+                        string content = textBox1.Text + Environment.NewLine;
+                        File.AppendAllText(filePath, content);
+                    }
+                    else
+                    {
+                        textBox1.Clear();
+                        return;
+                    }
+                }
+                else
+                {
+                    tasksbox.Items.Add(textBox1.Text);
+                    string content = textBox1.Text + Environment.NewLine;
+                    File.AppendAllText(filePath, content);
+                }
+            }
         }
 
         private void deltask_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void savenexit_Click(object sender, EventArgs e)
-        {
-
+            ListBox.SelectedObjectCollection selectedTask = new ListBox.SelectedObjectCollection(tasksbox);
+            selectedTask = tasksbox.SelectedItems;
+            if (tasksbox.SelectedIndex != -1)
+            {
+                for (int i = selectedTask.Count - 1; i >= 0; i--)
+                {
+                    tasksbox.Items.Remove(selectedTask[i]);
+                    //ADD: REMOVING FROM TXT FILE...
+                }
+            }
         }
     }
 }
